b5e11b7e5a52582d913d6b143d896159199127d3
Michael Colburn <mac002@theColburns.us>
2018-01-19 15:55:08 -0500

    Adding Gitlab Utils

---
+++ ioc.liturgical.utils/src/main/java/org/ocmc/ioc/liturgical/utils/GitLabUtils.java
@@ -0,0 +1,20 @@
+package org.ocmc.ioc.liturgical.utils;
+
+import org.gitlab4j.api.GitLabApi;
+
+public class GitLabUtils {
+	public GitLabApi gitLabApi = null;
+	
+	public GitLabUtils (String url, String token) {
+		this.gitLabApi = new GitLabApi(url, token);
+	}
+
+	public GitLabApi getGitLabApi() {
+		return gitLabApi;
+	}
+
+	public void setGitLabApi(GitLabApi gitLabApi) {
+		this.gitLabApi = gitLabApi;
+	}
+	
+}
